91539270d750245b2e5351d0e636ec6c584d7c2f
Harold Reyes <haroldreyes@scien.tech>
2020-02-19 03:26:04 -0300

    environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
-export const PREFIX_DOMAIN_API =  'http://localhost:5000/api/';
+export const PREFIX_DOMAIN_API =  'https://site2020-api.azurewebsites.net/api/';
 export const environment = {
     production: false,
     hmr       : false,
