598081e79c9686d41ca1e5946941cf8598b1b0d1
Victor Cabas <victor.cabas@gmail.com>
2020-05-16 20:03:26 -0300

    url productiva de api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 //export const PREFIX_DOMAIN_API =  'http://localhost:5000/api/';
-export const PREFIX_DOMAIN_API =  'https://site2020-api.azurewebsites.net/api/';
+//export const PREFIX_DOMAIN_API =  'https://site2020-api.azurewebsites.net/api/';
+export const PREFIX_DOMAIN_API =  'https://cpauorgapi.azurewebsites.net/api/';
 //export const PREFIX_INSTITUCIONAL = 'http://localhost:50501/Content/institucional';
 export const PREFIX_INSTITUCIONAL = '/Content/institucional';
 //export const PREFIX_OLDSITE = 'http://localhost:50501';
