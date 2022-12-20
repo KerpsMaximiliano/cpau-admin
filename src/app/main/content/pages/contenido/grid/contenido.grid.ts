0d7d95610950cfb6b7b6a0a008a4cacee0a5aec0
Avaco Sistemas <avacosistemas@gmail.com>
2022-12-20 11:24:30 -0300

    ajustes url preview

--- src/app/main/content/pages/contenido/grid/contenido.grid.ts
+++ src/app/main/content/pages/contenido/grid/contenido.grid.ts
@@ -177,7 +177,7 @@ export const CONTENIDO_GRID_DEF: GridDef = {
         externalUrl: true,
         openTab: true,
         idUrl: true,
-        url: PREFIX_INSTITUCIONAL +  'notaPreview',
+        url: PREFIX_INSTITUCIONAL +  '/notaPreview',
         querystring: {
           id : 'id'
         }
