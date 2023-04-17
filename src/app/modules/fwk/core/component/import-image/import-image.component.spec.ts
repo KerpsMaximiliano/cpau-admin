d47f9922b38f5137021d8b40b3c473a4a9b885de
Alexis Ocampo <alexisocampo@MacBook-Pro-de-Alexis.local>
2023-04-16 22:43:06 -0300

    update cli y core

--- src/app/modules/fwk/core/component/import-image/import-image.component.spec.ts
+++ src/app/modules/fwk/core/component/import-image/import-image.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ImportImageComponent } from './Import-Image.component';
 
@@ -6,7 +6,7 @@ describe('ImportImageComponent', () => {
   let component: ImportImageComponent;
   let fixture: ComponentFixture<ImportImageComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ImportImageComponent ]
     })
